e26329f00993c043ea4fcd3df932faeae836e143
Ikdek <jules.pauchet1g1@gmail.com>
2026-03-18 12:24:48 +0100

    refactor: separate CSS and JS into files for LandingPage

--- LandingPage/server.js
+++ LandingPage/server.js
@@ -4,15 +4,35 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+const MIME_TYPES = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.ico': 'image/x-icon'
+};
+
 const server = http.createServer((req, res) => {
-    let filePath = path.join(__dirname, 'index.html');
+    let urlPath = req.url === '/' ? '/index.html' : req.url;
+    let filePath = path.join(__dirname, urlPath);
     
+    let ext = path.extname(filePath);
+    let contentType = MIME_TYPES[ext] || 'application/octet-stream';
+
     fs.readFile(filePath, (err, content) => {
         if (err) {
-            res.writeHead(500);
-            res.end(`Error: ${err.code}`);
+            if (err.code === 'ENOENT') {
+                res.writeHead(404);
+                res.end('404 File Not Found');
+            } else {
+                res.writeHead(500);
+                res.end(`500 Server Error: ${err.code}`);
+            }
         } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.writeHead(200, { 'Content-Type': contentType });
             res.end(content, 'utf-8');
         }
     });
