b621f2a9407f38123d4dbd1cf0fb05a62208c4cc
MabWalid <mabroukwalid0@gmail.com>
2026-03-19 10:28:30 +0100

    refactor:tests unitaires et coverage

--- PythonApi/darwin.py
+++ PythonApi/darwin.py
@@ -1,4 +1,5 @@
 from app import app
+import app.route  # noqa: F401  (enregistre les routes)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5002)
--- darwin.py
+++
@@ -1,5 +0,0 @@
-from app import app
-import app.route  # noqa: F401  (enregistre les routes)
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
