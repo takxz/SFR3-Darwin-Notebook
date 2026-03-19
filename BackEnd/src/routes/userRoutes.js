38dd404349be78b716ee7ab5ce267467d0d116a8
Ikdek <jules.pauchet1g1@gmail.com>
2026-03-19 10:29:50 +0100

    feat: add creatures routes and controller methods

--- BackEnd/src/routes/userRoutes.js
+++ BackEnd/src/routes/userRoutes.js
@@ -9,4 +9,10 @@ router.get('/profile', authMiddleware, userController.getProfile);
 // Route pour obtenir le profil public de quelqu'un d'autre
 router.get('/:id', authMiddleware, userController.getUserById);
 
+// Route pour ajouter une créature à l'utilisateur connecté
+router.post('/creatures/add', authMiddleware, userController.addCreature);
+
+// Route pour obtenir toutes les créatures d'un joueur par son ID
+router.get('/:id/creatures', authMiddleware, userController.getUserCreatures);
+
 module.exports = router;
