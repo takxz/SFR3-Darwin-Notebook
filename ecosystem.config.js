8b9443fce59a567caa91fdc1d2169a99e4d7dd82
Ikdek <jules.pauchet1g1@gmail.com>
2026-03-19 11:14:11 +0100

    Venvpathe

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       name: 'darwin-python-api',
       cwd: './PythonApi',
       script: 'darwin.py',
-      interpreter: './venv/bin/python',
+      interpreter: 'venv/bin/python',
     },
     {
       name: 'darwin-expo-develop',
