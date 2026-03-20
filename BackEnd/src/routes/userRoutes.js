5f7cfa0e0642b6c7678ddd45ff23a40d243daab3
amon <sachagorecki254@gmail.com>
2026-03-20 09:20:22 +0100

    feat: Ajout de l'upload photo

--- BackEnd/src/routes/userRoutes.js
+++ BackEnd/src/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const upload = require('../middlewares/uploadMiddleware');
 
 // Route protégée pour obtenir son propre profil
 router.get('/profile', authMiddleware, userController.getProfile);
@@ -9,8 +10,11 @@ router.get('/profile', authMiddleware, userController.getProfile);
 // Route pour obtenir le profil public de quelqu'un d'autre
 router.get('/:id', authMiddleware, userController.getUserById);
 
-// Route pour ajouter une créature à l'utilisateur connecté
-router.post('/creatures/add', authMiddleware, userController.addCreature);
+// Route pour ajouter une créature à l'utilisateur connecté avec upload d'image
+router.post('/creatures/add', authMiddleware, upload.single('image'), userController.addCreature);
+
+// Upload d'image de créature
+router.post('/creatures/upload', authMiddleware, upload.single('image'), userController.uploadCreatureImage);
 
 // Route pour obtenir toutes les créatures d'un joueur par son ID
 router.get('/:id/creatures', authMiddleware, userController.getUserCreatures);
