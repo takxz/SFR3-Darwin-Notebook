44013ec79268cfbee6e4a3b3b9a57551b50c27b4
20017435 <emeric.lesage@adeo.com>
2026-03-20 08:03:04 +0100

    updated FightScreenText to mock borders of the screen

--- FrontEnd/src/features/fight/FightScreen.test.jsx
+++ FrontEnd/src/features/fight/FightScreen.test.jsx
@@ -2,6 +2,11 @@ import { render, screen } from '@testing-library/react-native';
 import FightScreen from "./FightScreen";
 import fr from "@/assets/locales/fr.json";
 
+// Mock de useSafeAreaInsets pour éviter les erreurs liées à l'encoche lors du rendu du composant
+jest.mock('react-native-safe-area-context', () => ({
+    useSafeAreaInsets: () => ({ top: 40, bottom: 20, left: 0, right: 0 })
+}));
+
 describe("Page FightScreen", () => {
     it("Doit afficher le header et les deux boutons de l'arène", () => {
         render(
