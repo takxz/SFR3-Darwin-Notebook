f94035c29b740cf4cb3af61e37a8263776aa4e66
takxz <corentin.leys@gmail.com>
2026-04-20 15:57:33 +0200

    fix: i forgot to add a file on git

---
+++ FrontEnd/src/features/index/HomeFeedComponent.jsx
@@ -0,0 +1,31 @@
+import { View, StyleSheet } from "react-native";
+
+
+export default function HomeFeedComponent(){
+
+    
+
+
+
+
+
+
+
+    return (
+        <View style={styles.card}></View>
+
+    )
+}  
+
+const styles = StyleSheet.create({
+    card: {
+    backgroundColor: "#F8F3EA",
+    borderColor: "#E6DDCF",
+    borderWidth: 1,
+    borderRadius: 16,
+    paddingHorizontal: 14,
+    paddingTop: 12,
+    paddingBottom: 10,
+    width: "100%",
+  }
+})
