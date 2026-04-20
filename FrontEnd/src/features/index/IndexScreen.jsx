e15945d9f7bbd76709c0adf883e25bda2cb4d8f5
Axel <axelcastelot@gmail.com>
2026-04-20 15:16:02 +0200

    Ajoute une feature et Une page IndexScreen

---
+++ FrontEnd/src/features/index/IndexScreen.jsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import MapButton from "@/features/index/MapButton";
+
+export default function IndexScreen() {
+    return (
+        <View>
+            <MapButton />
+        </View>
+    );
+}
---
+++ FrontEnd/src/features/index/MapButton.jsx
@@ -0,0 +1,10 @@
+import { ArrowLeft, Map, Star, ChevronRight, Coins } from "lucide-react-native";
+import { Pressable } from "react-native";
+
+export default function MapButton() {
+    return (
+        <Pressable>
+            <Map />
+        </Pressable>
+    );
+}
