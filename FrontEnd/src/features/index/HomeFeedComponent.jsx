ca75bb435021c0f0f6eec196bc8f291c8f15a287
takxz <corentin.leys@gmail.com>
2026-04-21 09:56:23 +0200

    feat: call API to get last captured creature event in feed

--- FrontEnd/src/features/index/HomeFeedComponent.jsx
+++ FrontEnd/src/features/index/HomeFeedComponent.jsx
@@ -1,34 +1,17 @@
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import CardFeedComponent from "./CardFeedComponent";
-import { use, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function HomeFeedComponent() {
-  const mock_data = [
-    {
-    pseudo: "Soremus",
-    id: "fc12d64b-7061-425a-9d86-552bec8ff3d4",
-    player_id: "74923ac5-d0f2-48cc-a57a-e1e1121a6bdd",
-    gamification_name: "Fougère",
-    scan_url:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS64ZLcASghP_cbIHTV5sqS85lpY7yqHF_-fzQun0ViJLy-JMFM730XQtv4RTA1vSc-Y6waruDDvrRWfwjEvkVBVbAkqepBMxPuo2RLnA&s=10",
-    scan_quality: 1,
-    gps_location: null,
-    scan_date: "2026-04-20T16:34:46.423Z",
-  },
-    {
-      pseudo: "Soremus",
-      id: "ac160e56-a3de-4842-a15f-4a0b8f718378",
-      player_id: "74923ac5-d0f2-48cc-a57a-e1e1121a6bdd",
-      gamification_name: "Alpaga",
-      scan_url: "https://www.fermeexotique.fr/public/img/medium/nos-animaux-amerique-centrale-et-du-sud53ec8e5e82cdb.jpg",
-      scan_quality: 1,
-      gps_location: null,
-      scan_date: "2026-04-20T14:36:50.808Z",
-    },
-  ];
+    const [details, setDetails] = useState(null);
 
   useEffect(() => {
-    // TODO : remplacer le mock_data par un fetch vers le backend pour récupérer les dernières créatures capturées
+    fetch("http://ikdeksmp.fr:12000/api/user/creatures/last-captured")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Données reçues du backend :", data);
+        setDetails(data);
+      });
   }, []);
 
   const formatDate = (isoDate) =>
@@ -48,17 +31,21 @@ export default function HomeFeedComponent() {
       showsVerticalScrollIndicator={false}
     >
       <View style={styles.card}>
-        {mock_data.map((item) => (
-          <CardFeedComponent
-            key={item.id}
-            pseudo={item.pseudo}
-            animal_name={item.gamification_name}
-            scan_url={item.scan_url}
+        {details ? (
+          details.map((item) => (
+            <CardFeedComponent
+              key={item.id}
+              pseudo={item.pseudo}
+              animal_name={item.gamification_name}
+              scan_url={item.scan_url}
             gps_location={item.gps_location}
             scan_quality={item.scan_quality}
             scan_date={formatDate(item.scan_date)}
           />
-        ))}
+        ))
+      ) : (
+        <Text>Chargement des données...</Text>
+      )}
       </View>
     </ScrollView>
   );
